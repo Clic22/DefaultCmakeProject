63907a4c647c0020037084f7c28638c6aaf8fb03
Aymeric Meindre <aym.meindre@gmail.com>
2022-07-02 11:57:16 +0200

    add gitIgnore and change default name project

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ameindre/code/P2P/test
-# Build directory: /home/ameindre/code/P2P/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/ameindre/code/P2P/build/test/tests[1]_include.cmake")
--- build/test/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/ameindre/code/P2P/build/test/tests[1]_tests.cmake")
-  include("/home/ameindre/code/P2P/build/test/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
